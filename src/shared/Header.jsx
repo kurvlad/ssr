d8aae2d28c8ab6052b1644686e38a512e4f61437
kurvlad <kurvlad@mail.ru>
2023-02-01 18:04:42 +0300

    ssr

---
+++ src/shared/Header.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export function Header() {
+  return (
+    <header>
+      <h1>Reddit for our own from header.jsx</h1>
+      <h2>test second ts</h2>
+    </header>
+  )
+}
